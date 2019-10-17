fc8533469e4b5836c63f150f9cb46aee831a1fd5
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-16 21:16:14 -0400

    - Added the footer and it's styles   - Including the @import statement for the MainContent section(not finished yet)

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text footer__text-left">Copyright 2019. Be Well Therapy.</p>
+      {/* no copyright symbol? */}
+      <p className="footer__text footer__text-right">“Think Well, Feel Well, and Be Well”</p>
+    </footer>
+  );
+}
