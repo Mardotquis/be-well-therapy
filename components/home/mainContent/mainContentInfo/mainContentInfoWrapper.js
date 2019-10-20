94f7598902749b07b7603a16b791e3c159f83384
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-20 19:59:42 -0400

    - Adding close button to modals

--- components/home/mainContent/mainContentInfo/mainContentInfoWrapper.js
+++ components/home/mainContent/mainContentInfo/mainContentInfoWrapper.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useModal from '../../../modal/useModal';
 
 export default function MainContentInfoWrapper({ children }) {
+  const { toggleModal } = useModal();
   return (
     <div className="home-maincontent__info ">
+      <button className="home-maincontent__info-close modal-close" onClick={toggleModal} type="button" aria-label="Close modal" />
       <div className="home-maincontent__info__top_lines">
         <div className="home-maincontent__info_lines_vertical" />
         <div className="home-maincontent__info_lines_horizontal" />
