ef3a91ebdbeaa65c4d88671393ba5545db371a51
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-20 19:58:16 -0400

    - Moved modal into new folder and added useModal custom hook

--- components/home/mainContent/cards/card.js
+++ components/home/mainContent/cards/card.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Modal from '../../../modal';
+import Modal from '../../../modal/modal';
+import useModal from '../../../modal/useModal';
 import MainContentInfo from '../mainContentInfo/mainContentInfo';
 
 export default function Card({ header = 'test header', backgroundImg = '/img/home/family.png', modalInfo }) {
   const parsedBackgroundImg = `url(${backgroundImg})`;
+  const { modalOpen, toggleModal } = useModal();
+
   return (
-    <div className="home-maincontent__cards__card">
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+    <div className="home-maincontent__cards__card" onClick={toggleModal} role="document" onKeyDown={toggleModal}>
       <div className="home-maincontent__cards__card-img" style={{ backgroundImage: parsedBackgroundImg }} />
       {/* <div className="home-maincontent__cards__card__text_wrapper">
         <h3 className="home-maincontent__cards__card__text_wrapper__lgtext">{header}</h3>
       </div> */}
-      <Modal modalToggle><MainContentInfo modalInfo={modalInfo} /></Modal>
+      <Modal modalOpen={modalOpen} toggleModal={toggleModal}><MainContentInfo modalInfo={modalInfo} /></Modal>
     </div>
   );
 }
