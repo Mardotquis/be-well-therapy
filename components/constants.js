78da665c94e498e98c60a82340febe3e25e73cba
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-26 17:17:46 -0400

    - Moved constants file up to the root of the components folder - Replaced hard-coded values in navbar with constants values

--- components/constants.js
+++ components/constants.js
@@ -44,8 +44,10 @@ export const hours = [
   },
 ];
 
-export const address = '1923 J N Pease Place Ste. 201 Charlotte, NC';
+export const address = '1923 J N Pease Place Ste. 201';
 
-export const email = '';
+export const email = 'something@email.com';// TODO - change later
 
-export const phoneNum = '7043343170';
+export const phoneNum = '704.334.3170';
+
+export const googleMapsLink = 'https://www.google.com/maps/dir//1923+J+N+Pease+Pl+STE+201,+Charlotte,+NC+28262/@35.3202514,-80.7752462,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x88541dc9a49fc7e9:0x75bc25c02ef4abc3!2m2!1d-80.7730575!2d35.3202514!3e0';
