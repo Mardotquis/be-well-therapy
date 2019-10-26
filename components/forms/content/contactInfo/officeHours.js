f789fac9764735d4f705d3e9b49dea470cf695d7
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-26 19:07:31 -0400

    - Adding key to .map() elements

--- components/forms/content/contactInfo/officeHours.js
+++ components/forms/content/contactInfo/officeHours.js
@@ -2,9 +2,11 @@ import React from 'react';
 import OfficeHoursListItem from './officeHoursListItem';
 import { hours } from '../../../constants';
 
+const random = require('uuid/v4');
+
 function displayHours() {
   return hours.map((info) => (
-    <OfficeHoursListItem day={info.abbr} start={info.start} end={info.end} />
+    <OfficeHoursListItem day={info.abbr} start={info.start} end={info.end} key={random()} />
   ));
 }
 
