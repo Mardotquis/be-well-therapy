0008b012984bd6fd7ffadffadf5f13b55417b306
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-31 00:34:46 -0400

    - Replacing div with header tag for hero

--- components/aboutUs/Hero.js
+++ components/aboutUs/Hero.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Hero() {
   return (
-    <div className="about_us__hero">
+    <header className="about_us__hero">
       <h1 className="about_us__hero-lgtext">Charlotte’s Top-Quality Professional Counseling Group</h1>
-    </div>
+    </header>
   );
 }
