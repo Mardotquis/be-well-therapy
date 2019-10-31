449e8e6afb86bf5e86e97aa383a522faa4e0631b
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-31 15:24:11 -0400

    - Adding headers backgroundImg paths to info

--- components/home/mainContent/cardsInfo.js
+++ components/home/mainContent/cardsInfo.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 export const cards = [
   {
+    header: 'Kids',
+    backgroundImg: '/img/home/small-girl.png',
+    subheader: '(Child/Teen)',
     modalInfo: {
       name: 'child-modal',
       mainSentence: 'Our child and adolescent therapists are trained to create solutions for:',
@@ -8,6 +11,8 @@ export const cards = [
     },
   },
   {
+    header: 'Individual',
+    backgroundImg: '/img/home/man-close-up.png',
     modalInfo: {
       name: 'individual-modal',
       mainSentence: 'Licensed professionals treat mental and emotional concerns and disorders through techniques designed to encourage communication of conflicts, insight into problems, and with the aim to improve the individual’s well-being.',
@@ -16,12 +21,16 @@ export const cards = [
     },
   },
   {
+    header: 'Family',
+    backgroundImg: '/img/home/family.png',
     modalInfo: {
       name: 'relationship-marriage-family-therapy',
       mainSentence: 'Sessions are designed to identify family or relationship patterns that contribute to the problems and help members break those habits. Relationship and family therapy involve discussions, improving communication, ways to relate with each other and problem-solving sessions with the family. Correct the things that are causing your family/relationship to not be as close and loving as you desire.',
     },
   },
   {
+    header: 'Clinical Supervision',
+    backgroundImg: '/img/home/writing.png',
     modalInfo: {
       name: 'clinical-supervision',
       mainSentence: 'Clinical supervision from the aspect of customizing this experience to enhance the supervisee’s professional development.  We provide a warm and comfortable environment that will allow supervisee to explore their thoughts, feelings, and behaviors as a counselor in order to best serve your client (s) and increase your effectiveness as a counselor.',
