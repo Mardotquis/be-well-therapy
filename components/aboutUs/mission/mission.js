5a2670a46c8bd6e5e3a40844010a02afe51f1fad
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-31 00:35:10 -0400

    - Adding Mission statement section and it's styles

---
+++ components/aboutUs/mission/mission.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Mission() {
+  /* eslint-disable max-len */
+  return (
+    <section className="about_us__mission">
+      <div className="about_us__mission_icon" />
+      <h2 className="about_us__mission-lgtext">Our Mission</h2>
+      <h3 className="about_us__mission-medtext">To provide professional counseling and wellness programs designed to heal, educate and empower people to live life in a more successful and pleasurable way.</h3>
+      <div className="about_us__mission_quote">
+        <p>Be Well Therapy, PLLC’s group of Licensed Therapists have decades of experience combined, specializing in working one-on-one with children, teenagers, adults, couples and families to create and maintain positive change in all aspects of life.</p>
+        <p>We believe if a magnifying glass was placed over every human’s life, an area that needs to be improved would be found. Therefore, whether an individual is experiencing a difficult time, ready to move in a new direction, achieve personal goals, have a mental disorder or illness, or desire to manage life easier, it is best to speak with a trained professional to help guide the steps to wellness. Be Well Therapy’s Therapists work with clients to identify, release and correct the things that are not working, and clients leave thinking clearer, feeling better, and empowered to make the necessary changes to improve their life.</p>
+      </div>
+    </section>
+  );
+}
--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Default from '../../layouts/default';
-import Hero from '../../components/aboutUs/Hero';
+import Hero from '../../components/aboutUs/hero';
+import Mission from '../../components/aboutUs/mission/mission';
 
 export default function AboutUs() {
   return (
     <Default meta={{ title: 'About Us' }}>
       <Hero />
+      <Mission />
     </Default>
   );
 }
