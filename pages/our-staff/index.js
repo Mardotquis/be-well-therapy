31eb2078a8045b32980f3f309714f4321eda9197
Marquis L. Mitchell <marquis0403@gmail.com>
2019-10-31 21:46:00 -0400

    - Adding beginning of Our Staff page including dynamic header

---
+++ pages/our-staff/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Default from '../../layouts/default';
+import Header from '../../components/header';
+
+export default function OurStaff() {
+  return (
+    <Default meta={{ title: 'Our Staff' }}>
+      <Header heading="Meet Our Team" backgroundHeading="Team" />
+    </Default>
+  );
+}
