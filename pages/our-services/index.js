0e447d22c709cab33bc31d5992443ed1dd78c15d
Marquis L. Mitchell <marquis0403@gmail.com>
2019-11-02 02:31:57 -0400

    - Adding beginning of the page

---
+++ pages/our-services/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Default from '../../layouts/default';
+import Header from '../../components/header';
+
+export default function Forms() {
+  return (
+    <Default meta={{ title: 'Forms' }}>
+      <Header heading="Our Services" backgroundHeading="Services" />
+    </Default>
+  );
+}
