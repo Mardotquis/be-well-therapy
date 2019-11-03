bf74b388230020bb534cf8822421e808983c0298
Marquis L. Mitchell <marquis0403@gmail.com>
2019-11-02 22:30:20 -0400

    - Fixing the title of the page

--- pages/our-services/index.js
+++ pages/our-services/index.js
@@ -3,9 +3,9 @@ import Default from '../../layouts/default';
 import Header from '../../components/header';
 import Cards from '../../components/ourServices/cards/cards';
 
-export default function Forms() {
+export default function OurServices() {
   return (
-    <Default meta={{ title: 'Forms' }}>
+    <Default meta={{ title: 'Our Services' }}>
       <Header heading="Our Services" backgroundHeading="Services" />
       <Cards />
     </Default>
