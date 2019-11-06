196cc3f8c32fd565a4dbc6467f182124095e098d
Marquis L. Mitchell <marquis0403@gmail.com>
2019-11-05 23:35:34 -0500

    Revert "- Adding full width for first card, followed by 2 x 2 layout for last few cards"
    This reverts commit 08edc1ebb3cb8463c6313e40f6ec7129bd72bc74.

--- components/ourStaff/employeeCards/employeeCards.js
+++ components/ourStaff/employeeCards/employeeCards.js
@@ -2,33 +2,9 @@ import React from 'react';
 import EmployeeCard from './employeeCard';
 import { employees } from '../staffInfo';
 
-const random = require('uuid/v4');
-
-const cards = employees.map((employee, index) => {
-  // helps give Rolanda's card the full width
-  if (index === 0) {
-    return (
-      <div className="employee_cards__first_card" key={random()}>
-        <EmployeeCard
-          name={employee.name}
-          certs={employee.certs}
-          headshotPath={employee.headshotPath}
-          modalInfo={employee.modalInfo}
-        />
-      </div>
-    );
-  }
-  // all others get normal width
-  return (
-    <EmployeeCard
-      key={random()}
-      name={employee.name}
-      certs={employee.certs}
-      headshotPath={employee.headshotPath}
-      modalInfo={employee.modalInfo}
-    />
-  );
-});
+const cards = employees.map((employee) => (
+  <EmployeeCard name={employee.name} certs={employee.certs} headshotPath={employee.headshotPath} modalInfo={employee.modalInfo} />
+));
 
 export default function EmployeeCards() {
   return (
