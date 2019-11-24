825e4b28e8c035828a526a4699a5aba226bd3d08
Marquis L. Mitchell <marquis0403@gmail.com>
2019-11-23 22:30:25 -0500

    - Adding extra small headshot circle img's src

--- components/ourStaff/staffInfo.js
+++ components/ourStaff/staffInfo.js
@@ -16,6 +16,7 @@ export const employees = [
       ],
       long: true,
       smallHeadshotPath: '/img/our-staff/rolanda-gabriel-circle.png',
+      extraSmallHeadshotPath: '/img/our-staff/rolanda-gabriel-sm-circle.png',
     },
   },
   {
@@ -31,6 +32,7 @@ export const employees = [
         'Latoya is a National Certified Counselor (NCC) and a Licensed Professional Counselor (LPC). She earned a Bachelor of Arts degree in Criminal Justice from Stockton University and a Master of Science degree in Mental Health Counseling from Walden University.', 'Latoya began her work in the counseling profession at a crisis intervention unit, where she provided short-term behavioral health services to adults undergoing significant emotional or psychiatric distress. Latoya has experience working with adolescents, adults, couples, and families. She has provided clinical assessments, individual, couples, family, and group counseling, as well as, addiction recovery services which include, Relapse Prevention, Early Intervention and Intensive Outpatient (IOP).',
       ],
       smallHeadshotPath: '/img/our-staff/latoya-layne-circle.png',
+      extraSmallHeadshotPath: '/img/our-staff/latoya-layne-sm-circle.png',
     },
   },
   {
@@ -45,6 +47,7 @@ export const employees = [
         'Quis aliqua esse adipisicing esse esse velit esse tempor cillum laboris excepteur magna incididunt occaecat. Sint occaecat duis duis enim aliquip aliquip tempor dolore excepteur. Cillum amet Lorem occaecat adipisicing proident aute occaecat duis quis tempor pariatur ullamco reprehenderit dolor. Aliquip tempor ea laborum consectetur velit ullamco voluptate dolore tempor pariatur laboris. Nisi velit eiusmod veniam proident aliqua minim in sit tempor tempor non ipsum et reprehenderit.', 'Id eu duis et do veniam do ut aute fugiat quis amet qui minim est. Anim incididunt pariatur qui officia non consectetur quis in exercitation. Sit cillum nisi culpa elit ipsum proident fugiat excepteur aliquip sunt non tempor et cupidatat. Enim consectetur elit non id ipsum eiusmod aliqua. Adipisicing laboris laborum aute culpa velit fugiat duis in ad in do ipsum magna reprehenderit.', // TODO - replace with real text
       ],
       smallHeadshotPath: '/img/our-staff/nena-el-bey-circle.png',
+      extraSmallHeadshotPath: '/img/our-staff/nena-el-bey-sm-circle.png',
     },
   },
   {
@@ -59,6 +62,7 @@ export const employees = [
         'Rollis Miles III is a Licensed Professional Counselor Associate (LPCA), licensed in North Carolina. He completed his undergraduate studies at St. Bonaventure University, earning a Bachelor of Arts in Sociology. He holds a Master of Science degree from Warner University in Mental Health Counseling.', 'Rollis’s professional counseling experience consists of providing services for depression, anxiety, stress, trauma, relationship challenges, grief and loss, ADHD, family systemic issues, and other mental health and life concerns. Rollis has worked with various populations including children, adolescents, adults, families, and couples. Rollis has also worked with individuals from numerous cultural backgrounds, sexual orientations, and gender identifications. Rollis uses solution-focused techniques to assist individuals in identifying their goals for the future. Rollis focuses on the positive change in behaviors that will enable his clientele to operate optimally.', 'Rollis views therapy as a collaborative effort between clinician and client. Rollis is focused on helping the client in achieving healthy emotional and mental health functioning in a safe and supportive environment.',
       ],
       smallHeadshotPath: '/img/our-staff/rollis-miles-circle.png',
+      extraSmallHeadshotPath: '/img/our-staff/rollis-miles-sm-circle.png',
     },
   },
   {
@@ -73,6 +77,7 @@ export const employees = [
         'Rebecca Schrenker and I am currently a Licensed Clinical Social Worker- Associate (LCSWA) and am currently working towards completing my clinical license. I graduated from Appalachian State University with a degree in Psychology and a minor in Sociology. After obtaining my undergraduate degree I decided to continue my education and pursue a master’s degree in Social Work. When starting my master’s degree I knew I wanted to pursue a clinical license and work with children. Be Well Therapy provides the perfect environment to help me advance in my career and work with the clientele I have a passion for. When working with children I take a system’s theological approach keeping in mind that there are many different external and internal factors that influence how a child interacts and or behaves within their environment. In addition, to providing the skills to help the child navigate the multiple factors influencing them in a positive and healthy way.',
       ],
       smallHeadshotPath: '/img/our-staff/rebecca-schrenker-circle.png',
+      extraSmallHeadshotPath: '/img/our-staff/rebecca-schrenker-sm-circle.png',
     },
   },
 ];
