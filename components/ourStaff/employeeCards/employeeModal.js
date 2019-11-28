7dc9a65f0a6e1623bc422e00cc6e92ab5884169f
Marquis L. Mitchell <marquis0403@gmail.com>
2019-11-27 19:17:10 -0500

    - Adding new function to handle new custom bio - Adding small style changes to accompany this new bio

--- components/ourStaff/employeeCards/employeeModal.js
+++ components/ourStaff/employeeCards/employeeModal.js
@@ -5,12 +5,28 @@ const random = require('uuid/v4');
 export default function EmployeeModal({
   modalInfo, toggleModal, name, mobile,
 }) {
+  function displayText() {
+    const {
+      info, customInfo, customListItems, long,
+    } = modalInfo;
+    if (customInfo) {
+      return (
+        < >
+          <p>{customInfo[0]}</p>
+          {customListItems.map((item) => <li key={random()}>{item}</li>)}
+          <p>{customInfo[1]}</p>
+        </ >
+      );
+    }
+    return info.map((item) => <p key={random()} style={long && { lineHeight: '30px' }}>{item}</p>);
+  }
+
   return (
     <div className="employee_cards__modal">
       <button className="employee_cards__modal__close modal-close" onClick={toggleModal} type="button" aria-label="Close modal" />
       <img src={mobile ? modalInfo.extraSmallHeadshotPath : modalInfo.smallHeadshotPath} alt={name} className="employee_cards__modal-picture" />
       <div className="employee_cards__modal__text">
-        {modalInfo.info.map((text) => <p key={random()} style={modalInfo.long && { lineHeight: '30px' }}>{text}</p>)}
+        {displayText()}
       </div>
       <div className="employee_cards__modal__bottom_lines">
         <div className="employee_cards__modal_line_horizontal" />
