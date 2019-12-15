f7f1af30f2e07ff80eeb25b7d1966172c9bda727
Marquis L. Mitchell <marquis0403@gmail.com>
2019-12-15 15:52:11 -0500

    - Adding binary option for Node + Serverless

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const sls = require('serverless-http');
 const server = require('./server/index.js');
 
 // wrapping and exporting the server
-exports.server = sls(server);
+exports.server = sls(server,
+  // helps the server serve these files - because Node and binary files don't get along
+  { binary: ['application/*', 'image/*'] });
