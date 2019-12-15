002a0c6922e6fb7c9489d7849ef7ce15197cedd0
Marquis L. Mitchell <marquis0403@gmail.com>
2019-12-15 16:06:03 -0500

    - Updating transport variable to support both production and development/local enviorments - Grabbing new environment variables for dynamic transport - Updating HTML body and adding small minified CSS changes

--- server/contact/index.js
+++ server/contact/index.js
@@ -1,38 +1,63 @@
 const nodemailer = require('nodemailer');
 
 const {
-  HOST, CONTACT_EMAIL, CONTACT_PASS, EMAIL,
+  CURRENT_STAGE,
+  DEV_HOST, DEV_CONTACT_EMAIL, DEV_CONTACT_PASS, DEV_EMAIL,
+  PROD_HOST, PROD_CONTACT_EMAIL, PROD_CONTACT_PASS, PROD_EMAIL, PROD_SERVICE, PROD_RECIPIENT,
 } = process.env;
 
-const transport = nodemailer.createTransport({
-  host: HOST,
-  port: 2525,
-  auth: {
-    user: CONTACT_EMAIL,
-    pass: CONTACT_PASS,
-  },
-});
+let EMAIL;
+let transport;
+// recipient isn't needed for development
+let RECIPIENT = 'notneeded@email.com';
+
+if (CURRENT_STAGE === 'production') {
+  EMAIL = PROD_EMAIL;
+  RECIPIENT = PROD_RECIPIENT;
+  transport = nodemailer.createTransport({
+    service: PROD_SERVICE,
+    host: PROD_HOST,
+    auth: {
+      user: PROD_CONTACT_EMAIL,
+      pass: PROD_CONTACT_PASS,
+    },
+  });
+} else {
+  EMAIL = DEV_EMAIL;
+  transport = nodemailer.createTransport({
+    host: DEV_HOST,
+    port: 2525,
+    auth: {
+      user: DEV_CONTACT_EMAIL,
+      pass: DEV_CONTACT_PASS,
+    },
+  });
+}
+
 
 exports.sendEmail = async function sendEmail(body) {
   const {
     name, email, subject, message,
   } = body;
-  if (!name || !email) throw new Error('Missing body values: \n', body);
+  if (!name || !email) throw new Error('Missing required body values: \n', body);
 
   const formOutput = `
+  <style>*{font-family:Helvetica,sans-serif}h1{font-weight:600}h2{font-size:16px}h2 a, span{font-weight:400;font-size:14px}p{line-height:22px}</style>
+
   <h1>New Contact Request!</h1>
-      <ul>
-          <li>Name: ${name}</li>
-          <li>Email: ${email}</li>
-      </ul>
-  ${subject ? `<h2>Subject: ${subject}</h2>` : ''}
-  ${message ? `<h2>Message</h2>
-      <p>${message}</p>` : ''}
+
+  <h2>Name: <span>${name}</span></h2>
+  <h2>Email: <a href="mailto:${email}">${email}</a></h2>
+
+  ${subject ? `<h2>Subject: <span>${subject}</span></h2>` : ''}
+
+  ${message ? ` <h2>Message:</h2>
+  <p>${message}</p>` : ''}
   `;
 
   const mailOptions = {
     from: `"Be Well Therapy, PLLC website", <${EMAIL}>`,
-    to: 'marquis0403@gmail.com',
+    to: RECIPIENT,
     subject: 'New Contact Request',
     replyTo: `${email}`,
     html: formOutput,
