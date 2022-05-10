2714f18d67cf92fb89b7224e1217b3480e7f808e
Marquis L. Mitchell <marquis0403@gmail.com>
2022-05-09 20:29:43 -0400

    updating staff info file, adding new members, removing unused data

--- components/ourStaff/staffInfo.js
+++ components/ourStaff/staffInfo.js
@@ -6,8 +6,7 @@ export const employees = [
       'Licensed Professional Counselor Supervisor (LPCS)',
       'Licensed Professional Counselor (LPC)',
     ],
-    headshotPath: '/img/our-staff/rolanda-gabriel.png',
-    smHeadshotPath: '/img/our-staff/rolanda-gabriel-sm.png',
+    headshotPath: '/img/our-staff/rolanda-gabriel.jpg',
     modalInfo: {
       info: [
         'Rolanda Gabriel is a Licensed Professional Counselor Supervisor (LPCS) and Licensed Professional Counselor (LPC) and has directed a private group practice for eleven years. She was born and raised in Charlotte, NC and became the first female State Champion at David W. Butler High School. She completed her undergraduate studies at the University of North Carolina at Charlotte, earning a Bachelor of Arts in Criminal Justice and Sociology, where she groomed her desire to assist individuals prior to becoming involved with the Criminal Justice system. Her experiences at UNCC solidified her passion and calling to assist individuals towards wellness, where she then received her Master of Arts degree in Community Mental Health Counseling from Hampton University.',
@@ -15,8 +14,6 @@ export const employees = [
         'Rolanda’s professional experience includes sixteen years of providing therapy in the Outpatient, Intensive In-home and Residential settings. She has a gift of understanding the thoughts and feelings of others and allowing others to feel comfortable to talk about difficulties in their life and implementing necessary steps to achieve solutions. She utilizes person-centered, solution-focused and cognitive behavioral techniques. Her experience includes working with individuals of all ages to resolve problems, work through life transitions, and learn to live and experience life in a more successful and pleasurable way. She has the experience to handle a variety of needs, including, but not limited to: life stressors, family conflict, stress and anger management, mood swings, depression, bipolar, anxiety, and attaining personal goals.',
       ],
       long: true,
-      smallHeadshotPath: '/img/our-staff/rolanda-gabriel-circle.png',
-      extraSmallHeadshotPath: '/img/our-staff/rolanda-gabriel-sm-circle.png',
     },
   },
   {
@@ -25,14 +22,11 @@ export const employees = [
       'National Certified Counselor (NCC)',
       'Licensed Professional Counselor (LPC)',
     ],
-    headshotPath: '/img/our-staff/latoya-layne.png',
-    smHeadshotPath: '/img/our-staff/latoya-layne-sm.png',
+    headshotPath: '/img/our-staff/latoya-layne.jpg',
     modalInfo: {
       info: [
         'Latoya is a National Certified Counselor (NCC) and a Licensed Professional Counselor (LPC). She earned a Bachelor of Arts degree in Criminal Justice from Stockton University and a Master of Science degree in Mental Health Counseling from Walden University.', 'Latoya began her work in the counseling profession at a crisis intervention unit, where she provided short-term behavioral health services to adults undergoing significant emotional or psychiatric distress. Latoya has experience working with adolescents, adults, couples, and families. She has provided clinical assessments, individual, couples, family, and group counseling, as well as, addiction recovery services which include, Relapse Prevention, Early Intervention and Intensive Outpatient (IOP).',
       ],
-      smallHeadshotPath: '/img/our-staff/latoya-layne-circle.png',
-      extraSmallHeadshotPath: '/img/our-staff/latoya-layne-sm-circle.png',
     },
   },
   {
@@ -40,48 +34,153 @@ export const employees = [
     certs: [
       'Licensed Professional Counselor Associate (LPCA)',
     ],
-    headshotPath: '/img/our-staff/nena-el-bey.png',
-    smHeadshotPath: '/img/our-staff/nena-el-bey-sm.png',
+    headshotPath: '/img/our-staff/nena-el-bey.jpg',
     modalInfo: {
       customInfo: [
-        'Nena Ahmad El Bey Is a National Certified Counselor who completed her Bachelor’s degree in Psychology from University of Phoenix and  her Master’s Degree in Marriage and Family Therapy from Grand Canyon University. Nena is Licensed as a Professional Counselor and a Licensed Mental Health, and Certified Marriage Family Therapist. She also attended Delphi University where she became a Certified Spiritual and Meditation Guide Therapist. Nena works passionately to bring creative components that will allow her to connect with her client’s individual and  family needs to restore peace of mind. Four years of counseling experience across a variety of treatment settings include; private practice, and community based services. Nena serves children, adolescents, adults, couples, families, and groups in the following areas:',
-        'Nena is eclectic and uses a blend of Cognitive Behavioral, Motivational Interviewing, Play Therapy, Solution Focused, Reality Based, and psycho-education to individuals. A holistic approach is utilized by exploring with and educating clients about how mind, spirit and body are interconnected. Her theory is that when people have hope and can learn to challenge and replace irrational thoughts, beliefs, and attitudes, people are free to experience change. Nena thrives to support and guide her clients to a more positive mental health and wellness experience.',
+        `Nena Ahmad El Bey Is a National Certified Counselor who completed her Bachelor’s degree in
+        Psychology from University of Phoenix and her Master’s Degree in Marriage and Family Therapy from
+        Grand Canyon University. Nena is a Licensed Clinical Mental Health Counselor, and Certified Marriage
+        Family Therapist. She also attended Delphi University of Spitritual studies where she became a Certified
+        Spiritual and Meditation Guide Therapist, a Rohan Therapist, and an ordained Minister. Nena works
+        passionately to bring creative components that will allow her to connect with her client’s individual and
+         family needs to restore peace of mind. 7 years of counseling experience across a variety of treatment
+        settings include; private practice, and community based services. Nena serves children, adolescents,
+        adults, couples, families, and groups in the following areas:`,
+        `Nena is an eclectic therapist who prides herself on implementing holistic approaches to connect with
+        each client and assist throughout the healing journey. A number of modalities are incorporated in her
+        therapeutic style including Cognitive Behavioral, Motivational Interviewing, Play Therapy, Solution
+        Focused, Reality Based, psycho-education to individuals, energetic cleansing &; balancing, and
+        mindfulness to name a few . A holistic approach is utilized by exploring with and educating clients about
+        how mind, spirit and body are interconnected. Her theory is that when someone is supported and
+        guided to move through life’s challenges, there is a possibility to heal and truly accept, love and
+        celebrate the better version of self they are becoming. In a world with so many traumas, kayos and confusion Nena strives to support and guide her clients to a more positive mental health and wellness
+        experience.`,
       ],
-      customListItems: ['Addictive Behaviors', 'Teen Issues; Peer Pressure, Self Esteem', 'Parent/Child or Partner Relational Problems', 'Dating Violence/Abuse', 'Self-Harm Behavior', 'Obsessions and Compulsions', 'Mood Disorders', 'Personality Disorders', 'Anxiety Disorders', 'Conduct and Oppositional Defiant Disorders', 'Physical, Sexual, Emotional Abuse', 'Anger and Stress Management', 'Dual Diagnoses', 'Criminal Thinking and Behavior', 'Marriage and Family Therapy',
+      customListItems: [' Addictive Behaviors',
+        ' Teen Issues; Peer Pressure, Self Esteem',
+        ' Parent/Child or Partner Relational Problems',
+        ' Dating Violence/Abuse',
+        ' Self-Harm Behavior',
+        ' Obsessions and Compulsions',
+        ' Mood Disorders',
+        ' Personality Disorders',
+        ' Anxiety Disorders',
+        ' Conduct and Oppositional Defiant Disorders',
+        ' Physical, Sexual, Emotional Abuse',
+        ' Anger and Stress Management',
+        ' Dual Diagnoses',
+        ' Criminal Thinking and Behavior',
+        ' Marriage and Family Therapy',
+        'Grief and transitioning services for individuals and families',
       ],
       long: true,
-      smallHeadshotPath: '/img/our-staff/nena-el-bey-circle.png',
-      extraSmallHeadshotPath: '/img/our-staff/nena-el-bey-sm-circle.png',
     },
   },
   {
-    name: 'Rollis Miles, III, MA, LPCA',
+    name: 'Justina Ankrah',
     certs: [
-      'Licensed Professional Counselor Associate (LPCA)',
+      'Licensed Clinical Mental Health Counselor Associate (LCMHCA)',
     ],
-    headshotPath: '/img/our-staff/rollis-miles.png',
-    smHeadshotPath: '/img/our-staff/rollis-miles-sm.png',
+    headshotPath: '/img/our-staff/justina-ankrah.jpg',
     modalInfo: {
       info: [
-        'Rollis Miles III is a Licensed Professional Counselor Associate (LPCA), licensed in North Carolina. He completed his undergraduate studies at St. Bonaventure University, earning a Bachelor of Arts in Sociology. He holds a Master of Science degree from Warner University in Mental Health Counseling.', 'Rollis’s professional counseling experience consists of providing services for depression, anxiety, stress, trauma, relationship challenges, grief and loss, ADHD, family systemic issues, and other mental health and life concerns. Rollis has worked with various populations including children, adolescents, adults, families, and couples. Rollis has also worked with individuals from numerous cultural backgrounds, sexual orientations, and gender identifications. Rollis uses solution-focused techniques to assist individuals in identifying their goals for the future. Rollis focuses on the positive change in behaviors that will enable his clientele to operate optimally.', 'Rollis views therapy as a collaborative effort between clinician and client. Rollis is focused on helping the client in achieving healthy emotional and mental health functioning in a safe and supportive environment.',
+        `Justina Ankrah is a North Carolina Licensed Clinical Mental Health
+        Counselor Associate (LCMHCA), currently working towards full
+        clinical licensure. She earned her master’s degrees in Human
+        Development and Learning, and in Counseling both from The University
+        of North Carolina at Charlotte. She has over twenty-five years of
+        experience working with diverse population in the human services field
+        providing services for individuals and families in need. Justina’s
+        professional counseling experience consist of providing services for
+        relief of stress, trauma, grief and loss, ADHD, anxiety, depression, and
+        other mental health and life challenges. Justina uses a solution-focused
+        and cognitive-behavioral techniques to assist individuals in identifying
+        their goals for the future and exploring behaviors and circumstances that
+        have supported a pathway toward those goals. Justina’s aim as a
+        counselor is to collaborate with the individual and assist in achieving
+        growth with the goal of a lasting emotional and mental wellness.`,
       ],
-      smallHeadshotPath: '/img/our-staff/rollis-miles-circle.png',
-      extraSmallHeadshotPath: '/img/our-staff/rollis-miles-sm-circle.png',
     },
   },
   {
-    name: 'Rebecca Schrenker, MSW, LCSWA',
+    name: 'Stephanie Williams',
     certs: [
-      'Licensed Clinical Social Worker- Associate (LCSWA)',
+      'Licensed Clinical Social Work Associate (LCSWA)',
     ],
-    headshotPath: '/img/our-staff/rebecca-schrenker.png',
-    smHeadshotPath: '/img/our-staff/rebecca-schrenker-sm.png',
+    headshotPath: '/img/our-staff/stephanie-williams.jpg',
     modalInfo: {
       info: [
-        'Rebecca is currently a Licensed Clinical Social Worker- Associate (LCSWA) and is currently working towards obtaining her full clinical license. Rebecca graduated from Appalachian State University with a degree in Psychology and a minor is Sociology. After Rebecca obtained her undergraduate degree, she decided to pursue a master’s degree in Social Work. Rebecca knew early on while obtaining her master’s degree that she wanted to pursue her clinical license and work with children. Be Well Therapy provides the perfect environment to help Rebecca advance in her career and allows her to work with the clientele she is very passionate about, children. Rebecca takes a system’s theological approach when working in children which highlights that there are many different external and internal factors that influence how a child interacts and or behaviors within their environment. In addition, this theological approach along with the guidance of the therapist helps the child navigate the multiple factors in a positive and healthy way.',
+        `Stephanie is a Licensed Clinical Social Work Associate through the North Carolina Social Work
+        Certification and Licensure Board. She earned her Master’s degree in Social Work from The University of
+        Alabama. She operates from a strengths-based approach, empowering individuals to develop a stronger
+        sense of well-being. She has recently spent some time providing intensive in-home therapy services to
+        at-risk youth and families. She has experience with providing case management services, crisis
+        intervention, and outpatient therapy services that include individual, group, and family therapy. She also
+        has experience working with underserved communities. She is currently assisting Be Well Therapy, PLLC
+        with implementing the BeCalm fitness program that aims to empower youth through a combination of
+        mental health techniques, exercise, meditation, and yoga. Stephanie is passionate about promoting
+        overall wellness. She likes to utilize Cognitive Behavioral Therapy, Dialectical Behavior Therapy, and the
+        Trauma Informed Care approach to improve behavioral challenges and problems. She enjoys traveling,
+        exercising, sports, outdoor activities, and all things fashion.`,
       ],
-      smallHeadshotPath: '/img/our-staff/rebecca-schrenker-circle.png',
-      extraSmallHeadshotPath: '/img/our-staff/rebecca-schrenker-sm-circle.png',
+    },
+  },
+  {
+    name: 'Dajah Black',
+    certs: [
+      'Licensed Clinical Social Work Associate (LCSWA)',
+    ],
+    headshotPath: '/img/our-staff/dajah-black.jpg',
+    modalInfo: {
+      info: [
+        `Dajah Black graduated with a Master's in Social Work from Johnson C. Smith University in
+        2020 and became a Licensed Clinical Social Worker Associate shortly after. Dajah has
+        experience working with children 6 and up, women, and families. She is trauma informed and
+        utilizes a cognitive behavior therapy, strengths based approach in which we will focus on how
+        thoughts influence feelings, and behaviors as well as your strengths and self determination to
+        meet your desired goals. Although this is an approach Dajah typically utilizes, she understands
+        the importance of being flexible in meeting the client's needs as she will focus on rapport
+        building to ensure comfortability, transparency, and growth. She looks forward to being a
+        support for you!`,
+      ],
+    },
+  },
+  {
+    name: 'Holly McDonald',
+    certs: [
+      'Licensed Clinical Social Work Associate (LCSWA)',
+    ],
+    headshotPath: '/img/our-staff/holly-mcdonald.jpg',
+    modalInfo: {
+      info: [
+        `Holly McDonald is the Program Director with an  Undergraduate degree in Sociology and Ethnic
+        Studies and a master's degree in Social Work. Holly's professional counseling experience
+        consists of providing services for depression, anxiety, stress, trauma, relationship issues,grief and
+        loss, developmental disabilities, substance abuse, military family life.supports, career transitions,
+        hospice.. Holly has worked with various populations including children, adolescents, adults,
+        families, and couples. Holly's worked with individuals from a diverse ethnic &amp; cultural
+        population as well as backgrounds, along with sexual orientations, and gender identifications.
+        Holly uses psychodynamic counseling, solution-focused and cognitive-behavioral techniques to
+        assist individuals in identifying their goals and work through their challenges.`,
+      ],
+    },
+  },
+  {
+    name: 'Shaquasha Campbell',
+    headshotPath: '/img/our-staff/shaquasha-campbell.jpg',
+    modalInfo: {
+      info: [
+        'Shaquesha Campbell has been practicing social work for nearly 6 years with youth, adolescents, adults, and geriatrics. As a licensed clinical social work therapist, Shaquesha Campbell has been trained in Cognitive Behavioral Therapy (CBT), Dialectical behavioral therapy (DBT), Person Centered Planning, Solution Focused Therapy and Motivational Interviewing skills.  The majority of her practice consists of working with individuals who experience the following: Depression, Sexual Abuse, Suicidal Ideation, Trauma, Post-Traumatic Stress Disorder (PTSD), Bipolar, Substance use, Anxiety, Anger, Panic Attacks, marriage / relationship issues and parenting skills. Mrs. Campbell infuses evidenced based practices with client-centered strategies and techniques to help clients achieve real measurable and lasting change. She works primarily with individuals dealing with depression, anxiety, domestic violence, family conflicts and childhood traumas.',
+      ],
+    },
+  },
+  {
+    name: 'Janet Evans',
+    headshotPath: '/img/our-staff/janet-evans.jpg',
+    modalInfo: {
+      info: [
+        'Janet has a passion for billing, coding and office management.  She has over 22 years of experience working behind the scenes of keeping medical practices operating at their best capacity.',
+      ],
     },
   },
 ];
