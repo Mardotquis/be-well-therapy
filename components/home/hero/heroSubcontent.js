7aff0aedd93f117b84b2c4a05a3f0616c89a526f
Marquis L. Mitchell <marquis0403@gmail.com>
2022-05-25 01:16:31 -0400

    updating hero content

--- components/home/hero/heroSubcontent.js
+++ components/home/hero/heroSubcontent.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 export default function HeroSubcontent() {
-  const smallContent = `We Listen, help, save relationships, keep families together, keep people out of jail and hospitals, reduce depressive and anxiety symptoms, teach how to navigate successfully through
-  life challenges, teach how to correct negative thinking and behavior, create solutions to life problems, resolve conflict,  help people grieve and deal with losses, learn how to properly
-  express all emotions.`;
+  const smallContent = 'We can: listen, help, save relationships, keep families together, keep people out of jail and hospitals, reduce depressive and anxiety symptoms,  heal from traumatic experiences, teach how to navigate successfully through life challenges, teach how to correct negative thinking and behavior, create solutions to life problems, resolve conflict, help people grieve and deal with losses, teach how to express all emotions in a healthy way.';
 
   return (
     <div className="home_hero__subcontent">
