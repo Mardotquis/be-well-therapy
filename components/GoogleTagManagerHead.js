01679ef2b012d988033759c909d24e3cea76499d
Marquis Mitchell <marquis0403@gmail.com>
2023-09-18 21:56:11 -0400

    adding new google tag

---
+++ components/GoogleTagManagerHead.js
@@ -0,0 +1,19 @@
+import Script from 'next/script';
+
+const GoogleTagManagerHead = () => (
+const measurementId = 'UA-180172827-1';
+
+  <>
+    <Script src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} strategy="beforeInteractive" />
+    <Script strategy="beforeInteractive">{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', '${measurementId}');
+    `}</Script>
+  </>
+);
+
+export default GoogleTagManagerHead;
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import GoogleTagManagerHead from '../components/GoogleTagManagerHead';
+
+export default function Document () {
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <GoogleTagManagerHead />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
