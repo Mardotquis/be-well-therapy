87dfcedbfb9c8134b09901560501cab1af105f7b
Marquis Mitchell <marquis0403@gmail.com>
2023-09-18 22:33:04 -0400

    new doc

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,37 @@
-// pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import GoogleTagManagerHead from '../components/GoogleTagManagerHead';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function MyDocument() {
+class MyDocument extends Document {
+  render() {
 
-    return (
+      const GA_TRACKING_ID = 'UA-180172827-1';
+      
+      return (
       <Html>
         <Head>
-          <GoogleTagManagerHead />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
+  }
 }
+
+export default MyDocument;
